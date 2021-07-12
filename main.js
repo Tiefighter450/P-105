d4903aa06c7a12332657dbdb811d553cb6dd9329
Tiefighter450 <74991102+Tiefighter450@users.noreply.github.com>
2021-07-12 10:08:12 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+Webcam.set({
+    width: 300,
+    height: 170,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach(camera);
+
+function takeSnapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="capturedImage" src="' + data_uri + '"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Qm84XOLj-/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded');
+}
+
+function check() {
+    img = document.getElementById("capturedImage");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("objectName").innerHTML = results[0].label;
+        document.getElementById("objectAccuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
